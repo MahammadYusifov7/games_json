77bf4f76988c9bd3eb9fc247e5a74a4c73e2d701
MahammadYusifov <ysfmaqa7@gmail.com>
2026-03-09 15:04:28 +0400

    new

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ let data = [
     {
         "title": "The Witcher 3: Wild Hunt",
         "price": 39.99,
+        "disc" : "10%",
+        "newprice" : 35.991,
         "category": "Action RPG",
         "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJ0qp9dhZEw9xPPGne8q2Aa8Cd7uu6Z64a0A&s"
     },
@@ -36,6 +38,8 @@ let data = [
     {
         "title": "Fortnite",
         "price": 29.99,
+        "disc" : "50%",
+        "newprice" : 14.99,
         "category": "Battle Royale",
         "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROUYV09xrxL5B5O9DmMcigmVsg50hU6idHRg&s"
     },
@@ -54,6 +58,8 @@ let data = [
     {
         "title": "Assassin's Creed Odyssey",
         "price": 29.99,
+        "disc" : "30%",
+        "newprice" : 21.89,
         "category": "Action RPG",
         "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXmmmfJMVSmEWHknaDNNqe3WVq-N6YjxG35A&s"
     },
@@ -78,21 +84,53 @@ let data = [
 
 ]
 
-data.map(item =>
+data.map(item => {
+    let discountHTML = item.disc ? `
+        <div class="absolute bg-red-500 text-white p-2 rounded-lg top-4 left-4">
+            ${item.disc} OFF
+        </div>
+    ` : '' ;
+
     cards.innerHTML += `
         <div class="card1">
-            <div
-                class="bg-white shadow-sm border border-gray-200 p-2 w-full max-w-sm rounded-lg overflow-hidden mx-auto mt-4">
-                <div class="aspect-[3/2]">
-                    <img src="${item.img}"
-                        class="w-full h-full object-cover rounded-lg" />
+            <div class="bg-white flex flex-col overflow-hidden hover:shadow-md transition-all">
+                <div class="w-full bg-gray-50 relative">
+                    <a href="javascript:void(0)" class="block">
+                        <img src="${item.img}" alt="Product 1"
+                            class="w-full object-cover object-top aspect-[230/307]" />
+                    </a>
+
                 </div>
-                <div class="p-4 text-center">
-                    <h3 class="text-xl font-bold">${item.title}</h3>
-                    <p class="mt-3 text-xl leading-relaxed">${item.price} $</p>
-                    <p class="mt-3 text-md text-slate-500 leading-relaxed">${item.category}</p>
+                <div class="p-2 flex-1 flex flex-col">
+                    <div class="flex-1">
+                        <a href="javascript:void(0)" class="block border-0 outline-0">
+                            <h5 class="text-sm sm:text-base font-semibold text-slate-900 truncate">${item.title}
+                            </h5>
+                        </a>
+                        <p class="text-sm mt-1 text-slate-600 truncate">${item.category}</p>
+                        <div class="flex flex-wrap justify-between gap-2 mt-3">
+                            <div class="flex gap-2">
+                                <h6 class="text-sm sm:text-base font-bold text-slate-900">${item.newprice}$</h6>
+                                <h6 class="text-sm sm:text-base text-slate-600"><strike>${item.price}$</strike></h6>
+                            </div>
+                          
+                        </div>
+                    </div>
+                    <div class="flex items-center gap-2 mt-4">
+                        <div class="bg-pink-200 hover:bg-pink-300 w-12 h-9 flex items-center justify-center rounded-sm cursor-pointer"
+                            title="Wishlist">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16px" class="fill-pink-600 inline-block"
+                                viewBox="0 0 64 64">
+                                <path
+                                    d="M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06 0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 0 0 1 26.36-8.33 2 2 0 0 0 3.27 0A14.5 14.5 0 0 1 60 22.5c0 14.41-22.83 29.83-28 33.14Z"
+                                    data-original="#000000"></path>
+                            </svg>
+                        </div>
+                        <button type="button"
+                            class="text-sm font-medium px-2 cursor-pointer min-h-[36px] w-full bg-blue-600 hover:bg-blue-700 text-white tracking-wide ml-auto outline-0 border-0 rounded-sm">Add
+                            to cart</button>
+                    </div>
                 </div>
-            </div>
         </div>
     `
-)
+})
